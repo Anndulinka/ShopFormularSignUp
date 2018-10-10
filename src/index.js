bfded0996aaef56d7c0ad169eef662048b626a92
Anndulinka <43399723+Anndulinka@users.noreply.github.com>
2018-10-10 08:39:08 +0200

    chngessss
    somethimg changed!!!

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ document.getElementById("app").innerHTML = `
 <h1>Welcome to the SHOPing world!</h1>
 <div>
   Do not dare to go 
-  <a href="https://www.etsy.com/" target="_blank" rel="noopener noreferrer">there!</a>
+  <a href="https://www.etsy.com/" target="_blank" rel="noopener noreferrer">THERE!</a>
+  But come HERE and sign up with US!
 </div>
 `;
