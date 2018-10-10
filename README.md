40af3d96d655161715275c91cd16100b43bdbb3f
Anndulinka <43399723+Anndulinka@users.noreply.github.com>
2018-10-10 08:17:05 +0200

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# ShopFormularSignUp
-Created with CodeSandbox
---
+++ src/index.js
@@ -0,0 +1,7 @@
+document.getElementById("app").innerHTML = `
+<h1>Welcome to the SHOPing world!</h1>
+<div>
+  Do not dare to go 
+  <a href="https://www.etsy.com/" target="_blank" rel="noopener noreferrer">there!</a>
+</div>
+`;
